c1735c350cf40c8342a5b7d9dc5cbdab0d06fc8e
peterfust <pfust75@gmail.com>
2020-04-21 21:36:24 +0200

    Update geochecker.js

--- geochecker.js
+++ geochecker.js
@@ -1,7 +1,7 @@
 // ==UserScript==
-// @name         geockecker
+// @name         geockeckerBruteforce
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      1.0
 // @description  try to take over the world!
 // @author       You
 // @match        https://geocheck.org/geo_inputchkcoord.php*
@@ -11,38 +11,48 @@
 (function() {
     'use strict';
     /*
-    * Make sure to:
-    *   - fill in the parameters below
-    *   - flush the server side cache before each brute force attack :)
+    * Make sure to adjust the parameters below and to clear the localStorage 'currentCoords' entry before each run!
     */
+
+    // Select which item of the latitude and longitude should be incremented
     var incrementItemLat = "dec" // one of 'deg', 'min' or 'dec'
     var incrementItemLon = "dec" // one of 'deg', 'min' or 'dec'
+
+    // Value which is incremented
     var incrementLat = 1;
     var incrementLon = 1;
+
+    // Upper limit of incrementation (i.e. where script will stop to increment)
     const maxValueLat = 69;
     const maxValueLon = 679;
+
+
     var currentCoords = {};
     const initialCoords = {
-                    "latitude": {
-                        "northSouth": "N",
-                        "latdeg": 47,
-                        "latmin": 27,
-                        "latdec": 60
-                    },
-                    "longitude": {
-                        "eastWest": "E",
-                        "londeg": 8,
-                        "lonmin": 33,
-                        "londec": 670
-                    }
-                };
+        "latitude": {
+            "northSouth": "N",
+            "latdeg": 47,
+            "latmin": 27,
+            "latdec": 60
+        },
+        "longitude": {
+            "eastWest": "E",
+            "londeg": 8,
+            "lonmin": 33,
+            "londec": 670
+        }
+    };
+
     String.prototype.padFunction = function(padStr, len) {
         var str = this;
         while (str.length < len)
             str = padStr + str;
         return str;
     };
+
     function doCheck() {
+
+        // Fill in form with current coords
         if (currentCoords.latitude.northSouth == "N") {
             document.getElementsByName("lat")[0].checked = true;
         } else {
@@ -60,143 +70,101 @@
         document.getElementsByName("lonmin")[0].value = currentCoords.longitude.lonmin.toString().padFunction("0",3);
         document.getElementsByName("londec")[0].value = currentCoords.longitude.londec.toString().padFunction("0",3);
 
-        // Update coords on server before submitting the form
-        fetch('https://geochecker.herokuapp.com/coords', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json; charset=utf-8',
-            },
-            body: JSON.stringify(currentCoords),
-        })
-            .then((response) => response.json())
-            .then((data) => {
-            console.log('Success:', data);
-            document.getElementsByName("geoform")[0].submit();
-        })
-            .catch((error) => {
-            console.error('Error:', error);
-        });
-    };
-    function getCoords() {
-        fetch('https://geochecker.herokuapp.com/coords')
-            .then((response) => {
-            return response.json();
-        })
-            .then((respData) => {
-            // create coords on server if not existing
-            if( Object.entries(respData).length === 0) {
-                fetch('https://geochecker.herokuapp.com/coords', {
-                    method: 'POST',
-                    headers: {
-                        'Content-Type': 'application/json; charset=utf-8',
-                    },
-                    body: JSON.stringify(initialCoords),
-                })
-                    .then((response) => response.json())
-                    .then((data) => {
-                    console.log('Success:', data);
-                    currentCoords = data;
-                    doCheck();
-                })
-                    .catch((error) => {
-                    console.error('Error:', error);
-                });
-            } else {
-                currentCoords = respData;
-                switch(incrementItemLon) {
-                    case "deg":
-                        if (currentCoords.longitude.londeg != maxValueLon) {
-                            currentCoords.longitude.londeg = currentCoords.longitude.londeg + incrementLon;
-                            doCheck();
-                        } else {
-                            switch(incrementItemLat) {
-                                case "deg":
-                                    if (currentCoords.latitude.latdeg != maxValueLat) {
-                                        currentCoords.latitude.latdeg = currentCoords.latitude.latdeg + incrementLat;
-                                        currentCoords.longitude = initialCoords.longitude;
-                                        doCheck();
-                                    }
-                                    break;
-                                case "min":
-                                    if (currentCoords.latitude.latmin != maxValueLat) {
-                                        currentCoords.latitude.latmin = currentCoords.latitude.latmin + incrementLat;
-                                        currentCoords.longitude = initialCoords.longitude;
-                                        doCheck();
-                                    }
-                                    break;
-                                case "dec":
-                                    if (currentCoords.latitude.latdec != maxValueLat) {
-                                        currentCoords.latitude.latdec = currentCoords.latitude.latdec + incrementLat;
-                                        currentCoords.longitude = initialCoords.longitude;
-                                        doCheck();
-                                    }
-                                    break;
+        // Calculate new coords for next usage
+        switch(incrementItemLon) {
+            case "deg":
+                if (currentCoords.longitude.londeg != maxValueLon) {
+                    currentCoords.longitude.londeg = currentCoords.longitude.londeg + incrementLon;
+                 } else {
+                    switch(incrementItemLat) {
+                        case "deg":
+                            if (currentCoords.latitude.latdeg != maxValueLat) {
+                                currentCoords.latitude.latdeg = currentCoords.latitude.latdeg + incrementLat;
+                                currentCoords.longitude = initialCoords.longitude;
                             }
-                        }
-                        break;
-                    case "min":
-                        if (currentCoords.longitude.lonmin != maxValueLon) {
-                            currentCoords.longitude.lonmin = currentCoords.longitude.lonmin + incrementLon;
-                            doCheck();
-                        } else {
-                            switch(incrementItemLat) {
-                                case "deg":
-                                    if (currentCoords.latitude.latdeg != maxValueLat) {
-                                        currentCoords.latitude.latdeg = currentCoords.latitude.latdeg + incrementLat;
-                                        currentCoords.longitude = initialCoords.longitude;
-                                        doCheck();
-                                    }
-                                    break;
-                                case "min":
-                                    if (currentCoords.latitude.latmin != maxValueLat) {
-                                        currentCoords.latitude.latmin = currentCoords.latitude.latmin + incrementLat;
-                                        currentCoords.longitude = initialCoords.longitude;
-                                        doCheck();
-                                    }
-                                    break;
-                                case "dec":
-                                    if (currentCoords.latitude.latdec != maxValueLat) {
-                                        currentCoords.latitude.latdec = currentCoords.latitude.latdec + incrementLat;
-                                        currentCoords.longitude = initialCoords.longitude;
-                                        doCheck();
-                                    }
-                                    break;
+                            break;
+                        case "min":
+                            if (currentCoords.latitude.latmin != maxValueLat) {
+                                currentCoords.latitude.latmin = currentCoords.latitude.latmin + incrementLat;
+                                currentCoords.longitude = initialCoords.longitude;
                             }
-                        }
-                        break;
-                    case "dec":
-                        if (currentCoords.longitude.londec != maxValueLon) {
-                            currentCoords.longitude.londec = currentCoords.longitude.londec + incrementLon;
-                            doCheck();
-                        } else {
-                            switch(incrementItemLat) {
-                                case "deg":
-                                    if (currentCoords.latitude.latdeg != maxValueLat) {
-                                        currentCoords.latitude.latdeg = currentCoords.latitude.latdeg + incrementLat;
-                                        currentCoords.longitude = initialCoords.longitude;
-                                        doCheck();
-                                    }
-                                    break;
-                                case "min":
-                                    if (currentCoords.latitude.latmin != maxValueLat) {
-                                        currentCoords.latitude.latmin = currentCoords.latitude.latmin + incrementLat;
-                                        currentCoords.longitude = initialCoords.longitude;
-                                        doCheck();
-                                    }
-                                    break;
-                                case "dec":
-                                    if (currentCoords.latitude.latdec != maxValueLat) {
-                                        currentCoords.latitude.latdec = currentCoords.latitude.latdec + incrementLat;
-                                        currentCoords.longitude = initialCoords.longitude;
-                                        doCheck();
-                                    }
-                                    break;
+                            break;
+                        case "dec":
+                            if (currentCoords.latitude.latdec != maxValueLat) {
+                                currentCoords.latitude.latdec = currentCoords.latitude.latdec + incrementLat;
+                                currentCoords.longitude = initialCoords.longitude;
                             }
-                        }
-                        break;
+                            break;
+                    }
                 }
-            }
-        });
+                break;
+            case "min":
+                if (currentCoords.longitude.lonmin != maxValueLon) {
+                    currentCoords.longitude.lonmin = currentCoords.longitude.lonmin + incrementLon;
+                } else {
+                    switch(incrementItemLat) {
+                        case "deg":
+                            if (currentCoords.latitude.latdeg != maxValueLat) {
+                                currentCoords.latitude.latdeg = currentCoords.latitude.latdeg + incrementLat;
+                                currentCoords.longitude = initialCoords.longitude;
+                            }
+                            break;
+                        case "min":
+                            if (currentCoords.latitude.latmin != maxValueLat) {
+                                currentCoords.latitude.latmin = currentCoords.latitude.latmin + incrementLat;
+                                currentCoords.longitude = initialCoords.longitude;
+                            }
+                            break;
+                        case "dec":
+                            if (currentCoords.latitude.latdec != maxValueLat) {
+                                currentCoords.latitude.latdec = currentCoords.latitude.latdec + incrementLat;
+                                currentCoords.longitude = initialCoords.longitude;
+                            }
+                            break;
+                    }
+                }
+                break;
+            case "dec":
+                if (currentCoords.longitude.londec != maxValueLon) {
+                    currentCoords.longitude.londec = currentCoords.longitude.londec + incrementLon;
+                } else {
+                    switch(incrementItemLat) {
+                        case "deg":
+                            if (currentCoords.latitude.latdeg != maxValueLat) {
+                                currentCoords.latitude.latdeg = currentCoords.latitude.latdeg + incrementLat;
+                                currentCoords.longitude = initialCoords.longitude;
+                            }
+                            break;
+                        case "min":
+                            if (currentCoords.latitude.latmin != maxValueLat) {
+                                currentCoords.latitude.latmin = currentCoords.latitude.latmin + incrementLat;
+                                currentCoords.longitude = initialCoords.longitude;
+                            }
+                            break;
+                        case "dec":
+                            if (currentCoords.latitude.latdec != maxValueLat) {
+                                currentCoords.latitude.latdec = currentCoords.latitude.latdec + incrementLat;
+                                currentCoords.longitude = initialCoords.longitude;
+                            }
+                            break;
+                    }
+                }
+                break;
+        }
+
+        // Store new coords
+        localStorage.setItem('currentCoords', JSON.stringify(currentCoords));
+
+        // Submit form
+        document.getElementsByName("geoform")[0].submit();
+    }
+
+    function getCoords() {
+        currentCoords = JSON.parse(localStorage.getItem('currentCoords'));
+        if (!currentCoords) {
+            currentCoords = initialCoords;
+        }
     }
     getCoords();
+    doCheck();
 })();
